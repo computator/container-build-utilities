5546130fc425e7725d03804a749b7759b6de03a1
Reuben Lifshay <rlifshay@gmail.com>
2018-07-31 01:37:32 -0700

    Update logging to remove builddir prefix.

--- src/umoci.lib.sh
+++ src/umoci.lib.sh
@@ -20,10 +20,10 @@ u_create_image () {
 	local usg="Usage: u_create_image"
 	local path
 	path=$(mktemp -udp "${BUILDDIR:-.}" image.XXXX)
-	u_log_info "Creating image '%s'" "$path"
+	u_log_info "Creating image '%s'" "${path#"${BUILDDIR:-.}/"}"
 	${UMOCI} init --layout "$path"
 	if [ $? -ne 0 ]; then
-		u_log_err "Failed to create image '%s'" "$path"
+		u_log_err "Failed to create image '%s'" "${path#"${BUILDDIR:-.}/"}"
 		return 1
 	fi
 	echo "$path"
@@ -35,15 +35,15 @@ u_clone_image () {
 	local usg="Usage: u_clone_image {src_name}"
 	local img path src
 	img="${1:?$usg}"
-	u_log_dbg "Cloning image '%s'" "$img"
+	u_log_dbg "Cloning image '%s'" "${img#"${BUILDDIR:-.}/"}"
 	if [ -d "${BUILDDIR:-.}/${img}" ]; then
 		src="${BUILDDIR:-.}/${img}"
-		u_log_dbg "Found image '%s'" "$src"
+		u_log_dbg "Found image '%s'" "${src#"${BUILDDIR:-.}/"}"
 		path=$(mktemp -dp "${BUILDDIR:-.}" imgclone.XXXX)
 		# reset mode
 		chmod $(printf %o $((0777 & ~$(umask)))) "$path"
-		u_log_dbg "Created image '%s'" "$path"
-		u_log_info "Cloning image '%s' to '%s'" "$src" "$path"
+		u_log_dbg "Created image '%s'" "${path#"${BUILDDIR:-.}/"}"
+		u_log_info "Cloning image '%s' to '%s'" "${src#"${BUILDDIR:-.}/"}" "${path#"${BUILDDIR:-.}/"}"
 		u_log_dbg "Copying image blobs"
 		# hardlink files in blobs to save space
 		cp -r -l "${src}/blobs" "$path"
@@ -56,13 +56,13 @@ u_clone_image () {
 		path=$(mktemp -dp "${BUILDDIR:-.}" imgclone.XXXX)
 		# reset mode
 		chmod $(printf %o $((0777 & ~$(umask)))) "$path"
-		u_log_dbg "Created image '%s'" "$path"
-		u_log_info "Cloning image '%s' to '%s'" "$src" "$path"
-		u_log_dbg "Unserializing image '%s'" "$src"
+		u_log_dbg "Created image '%s'" "${path#"${BUILDDIR:-.}/"}"
+		u_log_info "Cloning image '%s' to '%s'" "${src#"${BUILDDIR:-.}/"}" "${path#"${BUILDDIR:-.}/"}"
+		u_log_dbg "Unserializing image '%s'" "${src#"${BUILDDIR:-.}/"}"
 		tar -xzf "$src" -C "$path"
 		echo "$path"
 	else
-		u_log_err "Failed to clone image '%s': Not found!" "$img"
+		u_log_err "Failed to clone image '%s': Not found!" "${img#"${BUILDDIR:-.}/"}"
 		return 1
 	fi
 }
@@ -72,23 +72,23 @@ u_open_image () {
 	local usg="Usage: u_open_image {src_name}"
 	local img path src
 	img="${1:?$usg}"
-	u_log_dbg "Opening image '%s'" "$img"
+	u_log_dbg "Opening image '%s'" "${img#"${BUILDDIR:-.}/"}"
 	if [ -d "${BUILDDIR:-.}/${img}" ]; then
 		src="${BUILDDIR:-.}/${img}"
-		u_log_info "Opening image '%s'" "$src"
+		u_log_info "Opening image '%s'" "${src#"${BUILDDIR:-.}/"}"
 		echo "$src"
 	elif [ -f "${BUILDDIR:-.}/${img}.oci" ]; then
 		src="${BUILDDIR:-.}/${img}.oci"
 		path=$(mktemp -dp "${BUILDDIR:-.}" imgopen.XXXX)
 		# reset mode
 		chmod $(printf %o $((0777 & ~$(umask)))) "$path"
-		u_log_dbg "Created image '%s'" "$path"
-		u_log_info "Opening image '%s' as '%s'" "$src" "$path"
-		u_log_dbg "Unserializing image '%s'" "$src"
+		u_log_dbg "Created image '%s'" "${path#"${BUILDDIR:-.}/"}"
+		u_log_info "Opening image '%s' as '%s'" "${src#"${BUILDDIR:-.}/"}" "${path#"${BUILDDIR:-.}/"}"
+		u_log_dbg "Unserializing image '%s'" "${src#"${BUILDDIR:-.}/"}"
 		tar -xzf "$src" -C "$path"
 		echo "$path"
 	else
-		u_log_err "Failed to open image '%s': Not found!" "$img"
+		u_log_err "Failed to open image '%s': Not found!" "${img#"${BUILDDIR:-.}/"}"
 		return 1
 	fi
 }
@@ -100,7 +100,7 @@ u_set_image_name () {
 	local usg="Usage: u_set_image_name {image} {new_name}"
 	local newpath
 	newpath="$(dirname "${1:?$usg}")/${2:?$usg}"
-	u_log_info "Setting name for image '%s' to '%s'" "$1" "$newpath"
+	u_log_info "Setting name for image '%s' to '%s'" "${1#"${BUILDDIR:-.}/"}" "${newpath#"${BUILDDIR:-.}/"}"
 	mv -T "${1}" "$newpath"
 	echo "$newpath"
 }
@@ -109,7 +109,7 @@ u_clean_image () {
 	# Garbage collects unreferenced blobs in an image
 	local usg="Usage: u_clean_image {image}"
 	local img="${1:?$usg}"
-	u_log_info "Cleaning image '%s'" "$img"
+	u_log_info "Cleaning image '%s'" "${img#"${BUILDDIR:-.}/"}"
 	${UMOCI} gc --layout "$img"
 }
 
@@ -119,7 +119,7 @@ u_serialize_image () {
 	local img archive
 	img="${1:?$usg}"
 	archive="${BUILDDIR:-.}/${2:?$usg}.oci"
-	u_log_info "Serializing image '%s' as '%s'" "$img" "$archive"
+	u_log_info "Serializing image '%s' as '%s'" "${img#"${BUILDDIR:-.}/"}" "${archive#"${BUILDDIR:-.}/"}"
 	( cd "$img" && find -mindepth 1 -maxdepth 1 | sed 's:^./::' ) | \
 		tar -c --hard-dereference --numeric-owner --owner 0 --group 0 -z -f "$archive" -C "${1}" --files-from -
 	echo "$archive"
@@ -139,7 +139,7 @@ u_create_ref () {
 	local usg="Usage: u_create_ref {image}"
 	local ref
 	ref=$(u_gen_refname "${1:?$usg}")
-	u_log_info "Creating image ref '%s'" "$ref"
+	u_log_info "Creating image ref '%s'" "${ref#"${BUILDDIR:-.}/"}"
 	${UMOCI} new --image "$ref"
 	echo "$ref"
 }
@@ -150,7 +150,7 @@ u_clone_ref () {
 	local oldref tag
 	oldref=$(u_get_ref "${1:?$usg}" "${2:?$usg}")
 	tag=$(u_gen_refname)
-	u_log_info "Cloning image reference '%s' as '%s'" "$oldref" "$tag"
+	u_log_info "Cloning image reference '%s' as '%s'" "${oldref#"${BUILDDIR:-.}/"}" "$tag"
 	u_write_ref "$oldref" "$tag"
 	echo $(u_get_ref "$1" "$tag")
 }
@@ -166,7 +166,7 @@ u_list_refs () {
 	# Lists all the references in an image
 	local usg="Usage: u_list_refs {image}"
 	local img="${1:?$usg}"
-	u_log_dbg "Listing references in image '%s'" "$img"
+	u_log_dbg "Listing references in image '%s'" "${img#"${BUILDDIR:-.}/"}"
 	${UMOCI} list --layout "$img"
 }
 
@@ -174,7 +174,7 @@ u_write_ref () {
 	# Saves a copy of a reference under another name
 	local usg="Usage: u_write_ref {img_srcref} {refname}"
 	local imgref="${1:?$usg}" tag="${2:?$usg}"
-	u_log_info "Writing image ref '%s' to '%s'" "$imgref" "$tag"
+	u_log_info "Writing image ref '%s' to '%s'" "${imgref#"${BUILDDIR:-.}/"}" "$tag"
 	${UMOCI} tag --image "$imgref" "$tag"
 }
 
@@ -182,7 +182,7 @@ u_remove_ref () {
 	# Removes a reference from an image
 	local usg="Usage: u_remove_ref {imgref}"
 	local tag="${1:?$usg}"
-	u_log_info "Removing image ref '%s'" "$tag"
+	u_log_info "Removing image ref '%s'" "${tag#"${BUILDDIR:-.}/"}"
 	${UMOCI} rm --image "$tag"
 }
 
@@ -197,7 +197,7 @@ u_remove_refs_except () {
 		# remove image prefix (if any) when adding to the exclude list
 		exclude="${exclude} ${ref#*:}"
 	done
-	u_log_info "Removing image refs in image '%s' except for: %s" "$img" "$exclude"
+	u_log_info "Removing image refs in image '%s' except for: %s" "${img#"${BUILDDIR:-.}/"}" "$exclude"
 	for ref in $(u_list_refs "$img"); do
 		if ! echo "$exclude" | grep -qwF "$ref"; then
 			u_remove_ref $(u_get_ref "$img" "$ref")
@@ -214,7 +214,7 @@ u_open_layer () {
 	img="${1:?$usg}"
 	shift
 	bundle=$(mktemp -udp "${BUILDDIR:-.}" bundle.XXXX)
-	u_log_info "Unpacking image ref '%s' as '%s'" "$img" "$bundle"
+	u_log_info "Unpacking image ref '%s' as '%s'" "${img#"${BUILDDIR:-.}/"}" "$bundle"
 	${UMOCI} unpack --image "$img" "$bundle" "$@"
 	echo "$bundle"
 }
@@ -226,7 +226,7 @@ u_continue_layer () {
 	img="${2:?$usg}"
 	bundle="${1:?$usg}"
 	shift 2
-	u_log_info "Repacking changes from '%s' into image ref '%s'" "$bundle" "$img"
+	u_log_info "Repacking changes from '%s' into image ref '%s'" "$bundle" "${img#"${BUILDDIR:-.}/"}"
 	${UMOCI} repack --refresh-bundle --image "$img" "$bundle" "$@"
 }
 
@@ -237,7 +237,7 @@ u_close_layer () {
 	img="${2:?$usg}"
 	bundle="${1:?$usg}"
 	shift 2
-	u_log_info "Repacking changes from '%s' into image ref '%s'" "$bundle" "$img"
+	u_log_info "Repacking changes from '%s' into image ref '%s'" "$bundle" "${img#"${BUILDDIR:-.}/"}"
 	${UMOCI} repack --image "$img" "$bundle" "$@"
 	u_log_info "Removing '%s'" "$bundle"
 	rm -rf "$bundle"
@@ -251,7 +251,7 @@ u_config () {
 	local img
 	img="${1:?$usg}"
 	shift
-	u_log_info "Setting config options for image ref '%s': %s" "$img" "$*"
+	u_log_info "Setting config options for image ref '%s': %s" "${img#"${BUILDDIR:-.}/"}" "$*"
 	${UMOCI} config --image "$img" "$@"
 }
 
