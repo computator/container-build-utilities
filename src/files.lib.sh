f454f7b09177a3e546eede1a8f16d6ac8fbd316d
Reuben Lifshay <rlifshay@gmail.com>
2018-07-31 01:56:29 -0700

    Add logging to files library.

--- src/files.lib.sh
+++ src/files.lib.sh
@@ -9,11 +9,13 @@ u_get_asset () {
 	local uri scheme path fname cdir
 	uri="${1:?$usg}"
 	scheme="${uri%:*}"
+	u_log_dbg "Retrieving asset '%s' with scheme '%s'" "$uri" "$scheme"
 	case "$scheme" in
 		http|https)
 			fname=$(_u_uri_as_fname "$uri")
 			cdir="${ASSET_CACHEDIR:-${BUILDDIR:-.}/.assetcache}"
 			path="$cdir/$fname"
+			u_log_info "Storing asset '%s' as '%s'" "$uri" "$path"
 			if [ ! -f "$path" ]; then
 				[ -d "$cdir" ] || mkdir -p "$cdir"
 				wget -nv --show-progress -O "$path" "$uri"
@@ -27,10 +29,11 @@ u_get_asset () {
 			path="${uri#file://}"
 			;;
 		*)
-			echo "Unknown scheme '$scheme' in URI '$uri'" >&2
+			u_log_err "Unknown scheme '%s' in URI '%s'" "$scheme" "$uri"
 			return 1
 			;;
 	esac
+	u_log_dbg "Local asset path is: %s" "$path"
 	[ -f "$path" ] || return 1
 	echo "$path"
 }
@@ -42,12 +45,16 @@ u_extract () {
 	local usg="Usage: u_extract {archive_uri} {target_path)"
 	local archive="${1:?$usg}" target="${2:?$usg}"
 	local apath
+	u_log_info "Extracting archive '%s' to '%s'" "$archive" "$target"
 	apath="$(u_get_asset "$archive")"
+	u_log_dbg "Source archive path is: %s" "$apath"
 	mkdir -p "$target"
 	if [ "$(echo -n "${archive##*.}" | tr "[:upper:]" "[:lower:]")" = "zip" ]; then
+		u_log_dbg "Unzipping archive"
 		# handle zip
 		unzip -qq "$apath" -d "$target"
 	else
+		u_log_dbg "Untarring archive"
 		# assume file is a tar
 		tar -xf "$apath" -C "$target"
 	fi
